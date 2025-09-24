9dfff81c748e50cfe63513f9cced7d91cfa5449b
Christopher Nguyen <christophertn0099@gmail.com>
2025-09-24 18:27:36 -0400

    Merged product commit

--- lib/main.dart
+++ lib/main.dart
@@ -83,11 +83,6 @@ color: _getActiveColor(),
           label: const Text('Ignite'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
